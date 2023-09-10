7d94e75e67fabeced2aa261d2e48a31c7f764040
alexandros <alexandrosanat@gmail.com>
2023-09-10 12:43:42 +0100

    Adds template

--- playground/views.py
+++ playground/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+# In Django views are request handlers or Actions
 def say_hello(request):
-    return HttpResponse("Hello World")
+    return render(request, template_name="hello.html", context={"name": "Alex"})
