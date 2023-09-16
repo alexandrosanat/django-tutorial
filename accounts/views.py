22a93761751d64f7f9f39150993616c6dbd0e275
alexandros <alexandrosanat@gmail.com>
2023-09-16 20:14:27 +0100

    Adds user signup

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+
 
 # Create your views here.
 def signup_view(request):
-    return render(request, 'accounts/signup.html')
+    # First check if request is POST
+    if request.method == "POST":
+        # By passing the data back to a new instance of the form we
+        # are validating the data
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # log the user in
+            return redirect("articles:list")
+    else:
+        # If it's a GET request then go to the forms
+        form = UserCreationForm()
+    # This will still run if the if fails
+    return render(request, "accounts/signup.html", {"form": form})
