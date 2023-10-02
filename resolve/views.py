3ed926fbdf75f5939e2319d12166c675eb23fe29
alexandros <alexandrosanat@gmail.com>
2023-10-02 21:19:58 +0100

    Adds input output manipulation in the view

--- resolve/views.py
+++ resolve/views.py
@@ -1,8 +1,26 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
+def manipulate_input(input_text):
+    # Perform your manipulation logic here
+    # For example, you can convert the input to uppercase
+    return input_text.upper()
+
+
 def resolve_main(request):
+    user_input = ""
+    output = ""
+
+    if request.method == "POST":
+        user_input = request.POST.get("user_input", "")  # Retrieve the user's input
+
+        # Manipulate the user input before displaying it
+        output = manipulate_input(user_input)
+
+
     return render(
         request,
-        template_name="resolve_views/resolve.html",
+        "resolve_views/resolve.html",
+        {"user_input": user_input, "output": output},
     )
